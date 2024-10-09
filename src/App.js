8e1dbcb8348329bb915492a5ccb027dbd1b0c6a0
AbhishekKhobe522 <abhishekkhobe522@gmail.com>
2024-10-10 00:10:14 +0530

    Fix line ending issues

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+import React from 'react'
+import { getPosts, getRandomUsers } from './api'
+import PostCard from './components/PostCard';
+import UserCard from './components/UserCard';
 
 function App() {
+  const [data, setData]=useState(null);
+  const [users, setUsers] = useState(null);
+
+  useEffect (()=>{
+    getPosts().then((posts)=>setData(posts));
+  }, []);
+
+  useEffect(()=>{
+    getRandomUsers().then((user)=>setUsers(user.results[0]));
+  },[])
+  const refresh = ()=>
+  {getRandomUsers().then((user)=>setUsers(user.results[0]))};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users && <UserCard data={users}/>}
+      <button onClick={refresh}>Refresh Button</button>
+      {data ? data.map((e)=>  <PostCard title={e.title} body={e.body}/>) :<p>Not Data</p>}
     </div>
-  );
-}
+  )
+};
 
-export default App;
+export default App
